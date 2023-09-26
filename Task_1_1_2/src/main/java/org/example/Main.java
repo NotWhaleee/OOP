13e910890c447cc2b301783c0c47c480e1c58724
Nikita <n.kozorez@g.nsu.ru>
2023-09-26 11:50:41 +0700

    added git ignore

--- Task_1_1_2/src/main/java/org/example/Main.java
+++ Task_1_1_2/src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ package org.example;
 //import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) {
         Polynomial p1 = new Polynomial(new int[]{4, 3, 6, 7});
         Polynomial p2 = new Polynomial(new int[]{3, 2, 8});
