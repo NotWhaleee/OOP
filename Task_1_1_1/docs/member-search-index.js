298f162ca0409c346bdf437b081f377c93cea7cd
NotWhaleee <60616064+NotWhaleee@users.noreply.github.com>
2023-10-13 12:56:01 +0700

    Delete Task_1_1_1/docs directory

--- Task_1_1_1/docs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"my.pack","c":"Heapsort","l":"Heapsort()","url":"%3Cinit%3E()"},{"p":"my.pack","c":"Heapsort","l":"sort(int[])"}]
--- Task_1_1_1/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"my.pack","l":"Heapsort"}]
