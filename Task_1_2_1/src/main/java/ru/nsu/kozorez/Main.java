49fd3ab93f38f27fb646a30573ee1b292387fc35
Nikita Kozorez <n.kozorez@g.nsu.ru>
2023-10-17 11:18:12 +0700

    first  commit

---
+++ Task_1_2_1/src/main/java/ru/nsu/kozorez/Main.java
@@ -0,0 +1,21 @@
+package ru.nsu.kozorez;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+        Tree<String> tree = new Tree<>("R1");
+        var a = tree.addChild("A");
+        var b = a.addChild("B");
+        Tree<String> subtree = new Tree<>("R2");
+        subtree.addChild("C");
+        subtree.addChild("D");
+        tree.addChild(subtree);
+        b.remove();
+        System.out.println(tree);
+
+    }
+}
