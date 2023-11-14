d0a9e4412d3235f18ae19a4ba4ba7a2722fb428b
Nikita Kozorez <n.kozorez@g.nsu.ru>
2023-11-14 10:16:33 +0700

    codestyle fixes

---
+++ Task_1_3_1/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.kozorez","c":"FindSubstring","l":"find(String, String)","url":"find(java.lang.String,java.lang.String)"},{"p":"ru.nsu.kozorez","c":"FindSubstring","l":"FindSubstring()","url":"%3Cinit%3E()"}]
