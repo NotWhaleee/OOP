1246454ffd6271c96ce0c5856ad6bfbb0cf2e07c
Nikita Kozorez <n.kozorez@g.nsu.ru>
2023-12-25 17:54:38 +0700

    hard deadline

--- Task_1_5_2/src/main/java/ru/nsu/kozorez/Note.java
+++ Task_1_5_2/src/main/java/ru/nsu/kozorez/Note.java
@@ -4,38 +4,61 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * class for representing a note.
+ */
 public class Note {
 
-    private String title;
-    private String note;
-    final private Date date;
+    private final String title;
+    private final String note;
+    private final Date date;
 
+    /**
+     * constructor.
+     *
+     * @param title string.
+     * @param note string.
+     * @param date date.
+     */
     public Note(String title, String note, Date date) {
         this.title = title;
         this.note = note;
         this.date = date;
     }
 
+    /**
+     * get the text of the note.
+     *
+     * @return string.
+     */
     public String getNote() {
         return note;
     }
 
-    public void setNote(String note) {
-        this.note = note;
-    }
-
+    /**
+     * get the creation date.
+     *
+     * @return date.
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * get the title.
+     *
+     * @return string.
+     */
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
+    /**
+     * string representation of the note.
+     *
+     * @return string.
+     */
     @Override
     public String toString() {
         DateFormat dform = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
--- Task_1_5_2/src/main/java/ru/nsu/kozorez/Notebook.java
+++ Task_1_5_2/src/main/java/ru/nsu/kozorez/Notebook.java
@@ -1,61 +1,95 @@
 package ru.nsu.kozorez;
 
-
-import java.io.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayDeque;
 import java.util.Date;
 import java.util.Deque;
-import java.lang.reflect.Type;
 import java.util.List;
 import java.util.stream.Stream;
 
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
+/**
+ * notebook class.
+ */
 public class Notebook {
-    private Deque<Note> notes;// = new ArrayDeque<>();
+
+    private Deque<Note> notes;
 
     private final String jsonFile = "notebook.json";
 
+    /**
+     * constructor.
+     * uploads notes from the json file.
+     */
     public Notebook() {
         readFromJson();
     }
 
+    /**
+     * prints all the notes.
+     */
     public void show() {
         for (Note note : notes) {
             System.out.println(note);
         }
-
     }
 
-//    public void show(Date start, Date end, String[] keyWords) {
-//        for (Note note : notes) {
-//            if (note.getDate().getTime() >= start.getTime()
-//                    && note.getDate().getTime() <= end.getTime()) {
-//                for (String keyWord : keyWords) {
-//                    if (note.getTitle().toLowerCase().contains(keyWord.toLowerCase())) {
-//                        System.out.println(note);
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//    }
-public void show(Date start, Date end, String[] keyWords) {
-    Stream<Note> noteStream = notes.stream()
-            .filter(note -> isWithinDateRange(note, start, end))
-            .filter(note -> containsKeyword(note, keyWords));
+    /**
+     * converts all the notes to string.
+     *
+     * @return notes.
+     */
+    @Override
+    public String toString() {
+        String result = "";
+        for (Note note : notes) {
+            result = result.concat(note.toString());
+        }
+        return result;
+    }
 
-    noteStream.forEach(System.out::println);
-}
+    /**
+     * prints all the notes in the stated range with keywords in the title.
+     * @param start prints notes from this date.
+     * @param end prints notes up to that date.
+     * @param keyWords keywords in the title regardless of the register.
+     */
+    public void show(Date start, Date end, String[] keyWords) {
+        Stream<Note> noteStream = notes.stream()
+                .filter(note -> isWithinDateRange(note, start, end))
+                .filter(note -> containsKeyword(note, keyWords));
 
+        noteStream.forEach(System.out::println);
+    }
+
+
+    /**
+     * checks whether the note is within the specified date range.
+     *
+     * @param note note.
+     * @param start start date.
+     * @param end end date.
+     * @return true or false.
+     */
     private boolean isWithinDateRange(Note note, Date start, Date end) {
         long noteTime = note.getDate().getTime();
         return noteTime >= start.getTime() && noteTime <= end.getTime();
     }
 
+    /**
+     * checks whether the note contains stated keywords regardless of the register.
+     *
+     * @param note note.
+     * @param keyWords keywords. arr of strings.
+     * @return true or false.
+     */
     private boolean containsKeyword(Note note, String[] keyWords) {
         String noteTitleLower = note.getTitle().toLowerCase();
         return Stream.of(keyWords)
@@ -63,6 +97,13 @@ public void show(Date start, Date end, String[] keyWords) {
                 .anyMatch(noteTitleLower::contains);
     }
 
+    /**
+     * adds a note.
+     *
+     * @param title string.
+     * @param text string.
+     * @param currentDate date of creation.
+     */
     public void add(String title, String text, Date currentDate) {
         notes.push(new Note(title, text, currentDate));
         try {
@@ -72,10 +113,23 @@ public void show(Date start, Date end, String[] keyWords) {
         }
     }
 
-    public void delete(String title) { //removes all notes with provided title
+    /**
+     * removes all notes with provided title.
+     *
+     * @param title string.
+     */
+    public void delete(String title) {
         notes.removeIf(note -> note.getTitle().equals(title));
+        try {
+            writeToJson();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    /**
+     * uploads notes from the json.
+     */
     private void readFromJson() {
         Gson gson = new Gson();
         Type listType = new TypeToken<List<Note>>() {
@@ -89,7 +143,6 @@ public void show(Date start, Date end, String[] keyWords) {
                 // Create a Deque from the List
                 notes = new ArrayDeque<>(noteList);
 
-                // Now you can use the Deque 'notes'
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -98,6 +151,11 @@ public void show(Date start, Date end, String[] keyWords) {
         }
     }
 
+    /**
+     * writes notes to the json file.
+     *
+     * @throws IOException exception.
+     */
     private void writeToJson() throws IOException {
         GsonBuilder builder = new GsonBuilder();
         builder.setPrettyPrinting();
