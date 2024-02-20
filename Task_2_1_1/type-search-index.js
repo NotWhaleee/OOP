4166196a6ef996dbdd4afc480d09cba716f197b4
NotWhaleee <NotWhaleee@users.noreply.github.com>
2024-02-20 09:00:35 +0000

    Deploying to gh-pages from @ NotWhaleee/OOP@e607cc494a3df193f1731849c111dd58e8ccbd7e 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kozorez","l":"HasComposite"},{"p":"ru.nsu.kozorez","l":"HasCompositeStream"},{"p":"ru.nsu.kozorez","l":"HasCompositeThread"}]
