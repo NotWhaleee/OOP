3f5825a70ea5470771726bcb1b4945d86925aab3
notwhalee <kozorezn2004@gmail.com>
2024-03-18 23:07:11 +0700

    review dog barking

--- Task_2_2_1/src/test/java/CourierTest.java
+++ Task_2_2_1/src/test/java/CourierTest.java
@@ -1,10 +1,10 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import ru.kozorez.Courier;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.kozorez.Courier;
 
 /**
  * Test operations with courier.
--- Task_2_2_1/src/test/java/JsonTest.java
+++ Task_2_2_1/src/test/java/JsonTest.java
@@ -1,12 +1,10 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.IOException;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-
 import ru.kozorez.Json;
 import ru.kozorez.SetUpPizzeria;
 
