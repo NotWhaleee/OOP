3cfda24633d4286e38224d62fbbeb0a2974579c6
NotWhaleee <NotWhaleee@users.noreply.github.com>
2024-04-17 13:31:13 +0000

    Deploying to gh-pages from @ NotWhaleee/OOP@8eabf9ba792de9e473904295b26b78fce2813db9 🚀

---
+++ Task_2_3_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.kozorez","l":"Application"},{"p":"ru.kozorez","l":"Controller"},{"p":"ru.kozorez","l":"PopupController"},{"p":"ru.kozorez","l":"Snake"}];updateSearchResults();
