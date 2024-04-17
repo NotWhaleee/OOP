cf7674686675663b60299f7ce8e9ca60388d96aa
notwhalee <kozorezn2004@gmail.com>
2024-04-17 19:27:25 +0700

    fix git

--- Task_2_3_1/src/main/java/ru/kozorez/Application.java
+++ Task_2_3_1/src/main/java/ru/kozorez/Application.java
@@ -19,6 +19,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load());
         scene.getRoot().requestFocus();
         stage.setTitle("Sankey!");
